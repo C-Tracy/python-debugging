b716e2eaf7fcee73ff176de72545aedcb30e6482
Claire B. Tracy <claire5060@gmail.com>
2021-03-29 15:31:18 -0500

    Adding a fixed area of rectangle script using pdb to debug.

--- area_of_rectangle.py
+++ area_of_rectangle.py
@@ -29,8 +29,7 @@ def area_of_rectangle(height, width = None):
     >>> area_of_rectangle (7, 2)
     14
     """
-    if width:
-        width = height
+  
     area = height * width
     return area
 
@@ -41,11 +40,16 @@ if __name__ == '__main__':
                 "\tthe height of a square or the height and width of a "
                 "rectangle".format(script_name = sys.argv[0]))
         sys.exit(message)
+    
     height = sys.argv[1]
-    width = height
-    if len(sys.argv) > 3:
-        width = sys.argv[1]
+    if len(sys.argv) == 2:
+        width = height
+    if len(sys.argv) == 3:
+        height = sys.argv[1]
+        width = sys.argv[2]
 
+    height = int(height)
+    width = int(width)
     area = area_of_rectangle(height, width)
 
     message = "The area of a {h} X {w} rectangle is {a}".format(
